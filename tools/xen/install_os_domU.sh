be1b6b54845d3983cbc6f40cbd6781f029a82185
Mate Lakat <mate.lakat@citrix.com>
2013-07-11 18:36:55 +0100

    Cleanup moved to the beginning of installOSdomU
    Change-Id: I4261178556621162431cd14a96277c4b0d411d65

--- tools/xen/install_os_domU.sh
+++ tools/xen/install_os_domU.sh
@@ -39,6 +39,34 @@ THIS_DIR=$(cd $(dirname "$0") && pwd)
 source $THIS_DIR/xenrc
 
 #
+# Shutdown previous runs
+#
+
+DO_SHUTDOWN=${DO_SHUTDOWN:-1}
+CLEAN_TEMPLATES=${CLEAN_TEMPLATES:-false}
+if [ "$DO_SHUTDOWN" = "1" ]; then
+    # Shutdown all domU's that created previously
+    clean_templates_arg=""
+    if $CLEAN_TEMPLATES; then
+        clean_templates_arg="--remove-templates"
+    fi
+    ./scripts/uninstall-os-vpx.sh $clean_templates_arg
+
+    # Destroy any instances that were launched
+    for uuid in `xe vm-list | grep -1 instance | grep uuid | sed "s/.*\: //g"`; do
+        echo "Shutting down nova instance $uuid"
+        xe vm-unpause uuid=$uuid || true
+        xe vm-shutdown uuid=$uuid || true
+        xe vm-destroy uuid=$uuid
+    done
+
+    # Destroy orphaned vdis
+    for uuid in `xe vdi-list | grep -1 Glance | grep uuid | sed "s/.*\: //g"`; do
+        xe vdi-destroy uuid=$uuid
+    done
+fi
+
+#
 # Prepare Dom0
 # including installing XenAPI plugins
 #
@@ -107,33 +135,6 @@ fi
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 
-#
-# Shutdown previous runs
-#
-
-DO_SHUTDOWN=${DO_SHUTDOWN:-1}
-CLEAN_TEMPLATES=${CLEAN_TEMPLATES:-false}
-if [ "$DO_SHUTDOWN" = "1" ]; then
-    # Shutdown all domU's that created previously
-    clean_templates_arg=""
-    if $CLEAN_TEMPLATES; then
-        clean_templates_arg="--remove-templates"
-    fi
-    ./scripts/uninstall-os-vpx.sh $clean_templates_arg
-
-    # Destroy any instances that were launched
-    for uuid in `xe vm-list | grep -1 instance | grep uuid | sed "s/.*\: //g"`; do
-        echo "Shutting down nova instance $uuid"
-        xe vm-unpause uuid=$uuid || true
-        xe vm-shutdown uuid=$uuid || true
-        xe vm-destroy uuid=$uuid
-    done
-
-    # Destroy orphaned vdis
-    for uuid in `xe vdi-list | grep -1 Glance | grep uuid | sed "s/.*\: //g"`; do
-        xe vdi-destroy uuid=$uuid
-    done
-fi
 
 
 #
